796300269d52443a37cb5902ac18a43abdca66c2
Jamile Kerber <jamilekerber.dev@gmail.com>
2026-06-11 17:26:19 -0300

    Adicionado Client de email

---
+++ src/main/java/com/jamilekerber/bff_agendador_tarefas/business/EmailService.java
@@ -0,0 +1,17 @@
+package com.jamilekerber.bff_agendador_tarefas.business;
+
+import com.jamilekerber.bff_agendador_tarefas.business.dto.out.TarefasDTOResponse;
+import com.jamilekerber.bff_agendador_tarefas.infrastructure.client.EmailClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final EmailClient emailClient;
+
+    public void enviaEmail(TarefasDTOResponse dto) {
+        emailClient.enviarEmail(dto);
+    }
+}
